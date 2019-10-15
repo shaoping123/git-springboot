742aa10d6009e3ec78dedde60c227c1894d341bb
lishaoping <pingshao_li@163.com>
2019-10-15 14:56:59 +0800

    git-springboot 第一次commit

---
+++ git-springbootdemo/src/main/java/com/yyz/gitspringbootdemo/GitSpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.yyz.gitspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringbootdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringbootdemoApplication.class, args);
+    }
+
+}
